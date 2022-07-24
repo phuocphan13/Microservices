0fc8eb4f41941e9ec3f5d7c61efb013e35ade80e
phan phan <manhphuoc1995@gmail.com>
2022-07-24 15:37:24 +0700

    Create Catalog Context

---
+++ Services/Catalog/Catalog.API/Data/CatologContext.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public interface ICatologContext
+{
+    IMongoCollection<Product> Products { get; }
+}
+
+public class CatologContext : ICatologContext
+{
+    public IMongoCollection<Product> Products { get; }
+
+    public CatologContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+        Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        CatologContextSeed.SeedData(Products);
+    }
+
+
+    
+}
--- Services/Catalog/Catalog.API/Data/ICatologContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Catalog.API.Entities;
-using MongoDB.Driver;
-
-namespace Catalog.API.Data;
-
-public interface ICatologContext
-{
-    IMongoCollection<Product> Products { get; }
-}
