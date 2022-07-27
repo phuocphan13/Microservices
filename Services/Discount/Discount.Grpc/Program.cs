6db3dab1120aaa62e0bdaf1d3cef0560a5ede698
phan phan <manhphuoc1995@gmail.com>
2022-07-27 18:38:13 +0700

    Creat and Impletement Discount GRPC

---
+++ Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,25 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+
+var app = builder.Build();
+
+app.MigrateDatabase<Program>();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<DiscountService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    private readonly IDiscountRepository _repository;
+    private readonly ILogger<DiscountService> _logger;
+    private readonly IMapper _mapper;
+
+    public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger, IMapper mapper)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await _repository.GetDiscount(request.ProductName);
+
+        if (coupon is null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount with Product Name = {request.ProductName} is not existed"));
+        }
+
+        _logger.LogInformation($"Discount is retrieved for Product Name: {request.ProductName} with Amount {coupon.Amount}");
+
+        var couponModel = _mapper.Map<CouponModel>(coupon);
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = _mapper.Map<Coupon>(request.Coupon);
+        var result = await _repository.CreateDiscount(coupon);
+        if (!result)
+        {
+            _logger.LogError("Discount is failed created.");
+        }
+
+        _logger.LogInformation($"Discount is successfully created. Product Name: {coupon.ProductName}");
+        var couponModel = _mapper.Map<CouponModel>(coupon); 
+
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = _mapper.Map<Coupon>(request.Coupon);
+        var result = await _repository.UpdateDiscount(coupon);
+        if (!result)
+        {
+            _logger.LogError("Discount is failed updated.");
+        }
+
+        _logger.LogInformation($"Discount is successfully updated. Product Name: {coupon.ProductName}");
+        var couponModel = _mapper.Map<CouponModel>(coupon);
+
+        return couponModel;
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var result = await _repository.DeleteDiscount(request.ProductName);
+        if (!result)
+        {
+            _logger.LogError("Discount is failed deleted.");
+        }
+
+        _logger.LogInformation($"Discount is successfully deleted. Product Name: {request.ProductName}");
+
+        var response = new DeleteDiscountResponse()
+        {
+            Success = result
+        };
+
+        return response;
+    }
+}
