b9749bed8cf64b49c65726a1afd12798e801cda5
phan phan <manhphuoc1995@gmail.com>
2023-10-14 15:19:37 +0700

    Implement for EventBus common feature

--- EventBus.Messages/Events/IntegrationBaseEvent.cs
+++ EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -1,6 +1,22 @@
-﻿namespace EventBus.Messages.Events
+﻿using System;
+
+namespace EventBus.Messages.Events
 {
     public class IntegrationBaseEvent
     {
+        public Guid Id { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreationDate = createDate;
+        }
     }
 }
