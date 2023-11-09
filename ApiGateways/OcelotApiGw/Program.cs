fd12f7f4dc365a872c08c3c743f88a190d338b75
phan phan <manhphuoc1995@gmail.com>
2023-11-09 16:26:50 +0700

    Config for docker file and ocelot file

---
+++ ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,29 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder
+    .Host
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", false, true);
+    })
+    .ConfigureLogging((hostingContext, logginBuilder) =>
+    {
+        logginBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        logginBuilder.AddConsole();
+        logginBuilder.AddDebug();
+    });
+
+builder.Services
+    .AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+var app = builder.Build();
+
+await app.UseOcelot();
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
