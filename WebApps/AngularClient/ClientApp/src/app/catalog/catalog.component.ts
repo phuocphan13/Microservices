031da3ffb0bf34a53ded7b7d56f020d243c1cc0b
Tran Thuan Phat <thuanphat0611@gmail.com>
2023-12-04 17:10:17 +0700

    a part of catalog UI

--- WebApps/AngularClient/ClientApp/src/app/catalog/catalog.component.ts
+++ WebApps/AngularClient/ClientApp/src/app/catalog/catalog.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent {
 
--- WebApps/AngularClient/ClientApp/src/app/catalog/product-list/product-list.component.ts
+++ WebApps/AngularClient/ClientApp/src/app/catalog/product-list/product-list.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
 
