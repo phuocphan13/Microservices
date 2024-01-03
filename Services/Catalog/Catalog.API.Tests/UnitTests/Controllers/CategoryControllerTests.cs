609f1e95938b8b18ac6ec6874e3b51dcc8bb710c
Tran Thuan Phat <thuanphat0611@gmail.com>
2024-01-03 08:02:00 +0700

    add unit test for get category by name + by id controller

--- Services/Catalog/Catalog.API.Tests/UnitTests/Controllers/CategoryControllerTests.cs
+++ Services/Catalog/Catalog.API.Tests/UnitTests/Controllers/CategoryControllerTests.cs
@@ -7,6 +7,7 @@ using Catalog.API.Extensions;
 using Catalog.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using System.Runtime.CompilerServices;
 using Xunit;
 using ModelHelpers = Catalog.API.Tests.Common.ModelHelpers;
 
@@ -14,6 +15,7 @@ namespace Catalog.API.Tests.UnitTests.Controllers;
 
 public class CategoryControllerTests
 {
+    //GetCategories
     #region
     [Fact]
     public async Task GetCategories_ValidParams_ExpectedResult()
@@ -46,6 +48,100 @@ public class CategoryControllerTests
         var result = await controller.GetCategories(default);
 
         Assert.IsType<NotFoundResult>(result);
+    }
+    #endregion
+    //GetCategoryById
+    #region
+    [Fact]
+    public async Task GetCategoryById_ValidParams_ExpectedResult()
+    {
+        var categoryDetail = ModelHelpers.Category.GenerateCategory().ToDetail();
+
+        var categoryService = new Mock<ICategoryService>();
+        categoryService.Setup(x => x.GetCategoryByIdAsync(categoryDetail.Id!, default)).ReturnsAsync(new ApiDataResult<CategoryDetail>() { Data = categoryDetail });
+
+        var controller = new CategoryController(categoryService.Object);
+
+        var result = await controller.GetCategoryById(categoryDetail.Id!, default);
+
+        OkObjectResult okObjectResult = Assert.IsType<OkObjectResult>(result);
+
+        var data = Assert.IsType<ApiDataResult<CategoryDetail>>(okObjectResult.Value);
+
+        Assert.NotNull(data);
+    }
+
+    [Fact]
+    public async Task GetCategoryById_ValidParams_BadRequest()
+    {
+        var categoryService = new Mock<ICategoryService>();
+
+        var controller = new CategoryController(categoryService.Object);
+
+        var result = await controller.GetCategoryById(null!, default);
+
+        Assert.True(result is BadRequestResult || result is BadRequestObjectResult);
+    }
+
+    [Fact]
+    public async Task GetCategoryById_ValidParams_NotFound()
+    {
+        var categoryService = new Mock<ICategoryService>();
+
+        categoryService.Setup(x => x.GetCategoryByIdAsync("id", default)).ReturnsAsync((ApiDataResult<CategoryDetail>)null!);
+
+        var controller = new CategoryController(categoryService.Object);
+
+        var result = await controller.GetCategoryById("id", default);
+
+        Assert.True(result is NotFoundResult || result is NotFoundObjectResult);
+    }
+    #endregion
+    //GetCategoryByName
+    #region
+    [Fact]
+    public async Task GetCategoryByName_ValidParams_ExpectedResult()
+    {
+        var categoryDetail = ModelHelpers.Category.GenerateCategory().ToDetail();
+
+        var categoryService = new Mock<ICategoryService>();
+        categoryService.Setup(x => x.GetCategoryByNameAsync(categoryDetail.Id!, default)).ReturnsAsync(new ApiDataResult<CategoryDetail>() { Data = categoryDetail });
+
+        var controller = new CategoryController(categoryService.Object);
+
+        var result = await controller.GetCategoryByName(categoryDetail.Id!, default);
+
+        OkObjectResult okObjectResult = Assert.IsType<OkObjectResult>(result);
+
+        var data = Assert.IsType<ApiDataResult<CategoryDetail>>(okObjectResult.Value);
+
+        Assert.NotNull(data);
+    }
+
+    [Fact]
+    public async Task GetCategoryByName_InvalidParams_BadRequest()
+    {
+        var categoryService = new Mock<ICategoryService>();
+
+        var controller = new CategoryController(categoryService.Object);
+
+        var result = await controller.GetCategoryByName(null!, default);
+
+        Assert.True(result is BadRequestResult || result is BadRequestObjectResult);
+    }
+
+    [Fact]
+    public async Task GetCategoryByName_ValidParams_NotFound()
+    {
+        var categoryService = new Mock<ICategoryService>();
+
+        categoryService.Setup(x => x.GetCategoryByNameAsync("name", default)).ReturnsAsync((ApiDataResult<CategoryDetail>)null!);
+
+        var controller = new CategoryController(categoryService.Object);
+
+        var result = await controller.GetCategoryByName("name", default);
+
+        Assert.True(result is NotFoundResult || result is NotFoundObjectResult);
     }
     #endregion
 }
