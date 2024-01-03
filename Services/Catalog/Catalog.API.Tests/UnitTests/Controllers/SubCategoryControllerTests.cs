dff2f8e4512ae7224f3bdf0c3684c9895af31d97
Trung1496 <trung.nq1496@gmail.com>
2024-01-03 10:52:44 +0700

    Unit test SubCategory Controller

---
+++ Services/Catalog/Catalog.API.Tests/UnitTests/Controllers/SubCategoryControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using ApiClient.Catalog.SubCategory.Models;
+using ApiClient.Common;
+using Catalog.API.Controllers;
+using Catalog.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Tests.UnitTests.Controllers;
+
+public class SubCategoryControllerTests
+{
+    [Fact]
+    public async Task GetSubCategory_ValidParams_NotFound()
+    {
+        //Config mock data -- tao data ảo
+        var subCategoryService = new Mock<ISubCategoryService>();
+
+        subCategoryService.Setup(x => x.GetSubCategoriesAsync(default)).ReturnsAsync((ApiDataResult<List<SubCategorySummary>>)null!);
+
+        //Run test
+        var controller = new SubCategoryController(subCategoryService.Object);
+
+        var result = await controller.GetSubCategories(default);
+
+        Assert.IsType<NotFoundResult>(result);
+
+
+    }
+}
