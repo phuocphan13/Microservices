33ebe904de3f38b57aeea7d2736250b03093c078
Nguyễn Quốc Trung <trung.nq1496@gmail.com>
2024-01-12 21:40:57 +0700

    Unit test subcategory controller (#9)
    * Create CategoryDetail model + change somethings + Implement unit tests for CategoryController
    
    * add unit test for get category by name + by id controller
    
    * Unit test SubCategory Controller
    
    * add unit test for DeleteCategory Controller
    
    * Adding Visual Code Unit test
    
    * refactor some things, add some things
    
    * add some cases for CreateCategory Unit Test
    
    * Revert "add some cases for CreateCategory Unit Test"
    
    This reverts commit 6117e80fe54d378d6da2186a464f0f6f34b31591.
    
    * Refactor CategoryController, ResponseMessages, ModelHelpers. Add Create + Update for CategoryController Unit Test
    
    * add collection for CategoryControllerTests, refactor some asserts
    
    * UnitTest for SubCategory Get - GetById - GetByName - Create
    
    * Refactor code for CategoryControllerTests
    
    * implement unit test for categoryservice
    
    * add unit test Create + Update + Delete for categoryservice
    
    * fix value for categoryId
    
    * UnitTest for SubCategory Create - Update
    
    * Unit test for Delete SubCategory
    
    * Remove xunit.runner.visualstudio unusing
    
    ---------
    
    Co-authored-by: Tran Thuan Phat <thuanphat0611@gmail.com>
    Co-authored-by: phan phan <manhphuoc1995@gmail.com>

--- Services/Catalog/Catalog.API/Controllers/SubCategoryController.cs
+++ Services/Catalog/Catalog.API/Controllers/SubCategoryController.cs
@@ -10,18 +10,22 @@ namespace Catalog.API.Controllers;
 [Route("api/v1/[controller]/[action]")]
 public class SubCategoryController : ControllerBase
 {
-    private readonly ISubCategoryService _subCategory;
+    private readonly ISubCategoryService _subCategoryService;
 
-    public SubCategoryController(ISubCategoryService subCategory)
+    public SubCategoryController(ISubCategoryService subCategoryService)
     {
-        _subCategory = subCategory;
+        _subCategoryService = subCategoryService ?? throw new ArgumentNullException(nameof(subCategoryService));
     }
 
     [HttpGet]
     public async Task<IActionResult> GetSubCategories(CancellationToken cancellationToken)
     {
-        var result = await _subCategory.GetSubCategoriesAsync(cancellationToken);
-           
+        var result = await _subCategoryService.GetSubCategoriesAsync(cancellationToken);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
 
         return Ok(result);
     }
@@ -31,13 +35,13 @@ public class SubCategoryController : ControllerBase
     {
         if (string.IsNullOrWhiteSpace(id))
         {
-            return BadRequest("Missing Id.");
+            return BadRequest("Id cannot be empty.");
         }
 
-        var result = await _subCategory.GetSubCategoryByIdAsync(id, cancellationToken);
+        var result = await _subCategoryService.GetSubCategoryByIdAsync(id, cancellationToken);
 
         if (result is null)
-        { 
+        {
             return NotFound();
         }
 
@@ -45,15 +49,15 @@ public class SubCategoryController : ControllerBase
     }
 
     [HttpGet("{name}")]
-    public async Task<IActionResult> GetSubCategoryByName (string name, CancellationToken cancellationToken)
+    public async Task<IActionResult> GetSubCategoryByName(string name, CancellationToken cancellationToken)
     {
         if (string.IsNullOrWhiteSpace(name))
         {
             return BadRequest("Missing Name.");
         }
 
-        var result = await _subCategory.GetSubCategoryByNameAsync(name, cancellationToken);
-        if( result is null)
+        var result = await _subCategoryService.GetSubCategoryByNameAsync(name, cancellationToken);
+        if (result is null)
         {
             return NotFound();
         }
@@ -64,7 +68,27 @@ public class SubCategoryController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> CreateSubCategory([FromBody] CreateSubCategoryRequestBody requestBody, CancellationToken cancellationToken)
     {
-        var result = await _subCategory.CreateSubCategoryAsync(requestBody, cancellationToken);
+        if (requestBody is null)
+        {
+            return BadRequest("requestBody is null.");
+        }
+        
+        if (string.IsNullOrWhiteSpace(requestBody.Name))
+        {
+            return BadRequest("Name is null.");
+        }
+        
+        if (string.IsNullOrWhiteSpace(requestBody.SubCategoryCode))
+        {
+            return BadRequest("SubCategoryCode is null.");
+        }
+        
+        if (string.IsNullOrWhiteSpace(requestBody.CategoryId))
+        {
+            return BadRequest("CategoryId is null.");
+        }
+
+        var result = await _subCategoryService.CreateSubCategoryAsync(requestBody, cancellationToken);
 
         if (!result.IsSuccessCode)
         {
@@ -75,7 +99,6 @@ public class SubCategoryController : ControllerBase
 
             if (result.InternalErrorCode == 500)
             {
-
                 return Problem(result.Message);
             }
         }
@@ -86,11 +109,29 @@ public class SubCategoryController : ControllerBase
     [HttpPut]
     public async Task<IActionResult> UpdateSubCategory([FromBody] UpdateSubCategoryRequestBody requestBody, CancellationToken cancellationToken)
     {
-        var result = await _subCategory.UpdateSubCategoryAsync(requestBody, cancellationToken);
-
-        if (result is null)
+        if (requestBody is null)
         {
-            return Problem($"Cannot update product with name: {requestBody.Name}");
+            return BadRequest("requestBody is null.");
+        }
+        
+        if (string.IsNullOrWhiteSpace(requestBody.Id))
+        {
+            return BadRequest("Id is null.");
+        }
+
+        var result = await _subCategoryService.UpdateSubCategoryAsync(requestBody, cancellationToken);
+
+        if (!result.IsSuccessCode)
+        {
+            if (result.InternalErrorCode == 404)
+            {
+                return NotFound(result);
+            }
+
+            if (result.InternalErrorCode == 500)
+            {
+                return Problem(result.Message);
+            }
         }
 
         return Ok(result);
@@ -104,19 +145,25 @@ public class SubCategoryController : ControllerBase
             return BadRequest("Product Id is not allowed null.");
         }
 
-        var result = await _subCategory.DeleteSubCategoryAsync(id, cancellationToken);
+        var result = await _subCategoryService.DeleteSubCategoryAsync(id, cancellationToken);
 
         return Ok(result);
     }
 
     [HttpGet("{categoryId}")]
-    public async Task<IActionResult> GetSubCategoriesByCategoryId (string categoryId, CancellationToken cancellationToken)
+    public async Task<IActionResult> GetSubCategoriesByCategoryId(string categoryId, CancellationToken cancellationToken)
     {
-        if(string.IsNullOrWhiteSpace(categoryId))
+        if (string.IsNullOrWhiteSpace(categoryId))
         {
             return BadRequest("Missing CategoryId");
         }
-        var result = await  _subCategory.GetSubCategoriesByCategoryIdAsync(categoryId, cancellationToken);
+
+        var result = await _subCategoryService.GetSubCategoriesByCategoryIdAsync(categoryId, cancellationToken);
+        
+        if (result is null)
+        {
+            return NotFound();
+        }
 
         return Ok(result);
     }
