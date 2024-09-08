028bcc378d081405c0ec009c9464b19e21b7309b
phan phan <manhphuoc1995@gmail.com>
2024-09-08 23:20:13 +0700

    Reconfigure for OpenTelemetry

--- Common/Platform/Configurations/Options/OpenTelemetryOptions.cs
+++ Common/Platform/Configurations/Options/OpenTelemetryOptions.cs
@@ -5,7 +5,5 @@ public sealed class OpenTelemetryOptions
     public string ActivitySourceName { get; set; } = string.Empty;
     public string ServiceName { get; set; } = string.Empty;
     public string ServiceVersion { get; set; } = string.Empty;
-    public string ProthemousEndpoint { get; set; } = string.Empty;
-    public string JaegerEndpoint { get; set; } = string.Empty;
     public string Endpoint { get; set; } = string.Empty;
 }
